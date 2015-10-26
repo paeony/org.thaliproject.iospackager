460fd1e72acdada2d8a53ea611b077db53091432
tobe <tobybrad@microsoft.com>
2015-10-26 12:26:54 +0000

    Copy over www/* also

--- scripts/ios/before_build.sh
+++ scripts/ios/before_build.sh
@@ -4,4 +4,8 @@ mkdir www
 mkdir www/jxcore
 echo "var thali = require('thali');" > www/jxcore/app.js
 mv app/jxcore/thali.jx www/jxcore
+cp app/* www
+cp -R app/js www
+cp -R app/img www
+cp -R app/css www
 rm app/jxcore/thali.jxp
